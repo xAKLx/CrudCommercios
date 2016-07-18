0219946ff47ce3b244feb3965be21c652146dc21
xAKLx <xaklx20@gmail.com>
2016-07-18 11:11:25 -0400

    Initial Commit

---
+++ js/Model.js
@@ -0,0 +1,25 @@
+var comercios = {};
+
+//nombre, codigo y ubicacion son requeridos
+//ubicacion debe tener como atributos lat y lng
+function addComercio(nombre, codigo, descripcion, ubicacion) {
+    
+    if (nombre !== undefined && codigo !== undefined && ubicacion !== undefined && ubicacion.lat !== undefined && ubicacion.lng !== undefined) {
+        var comercio = {
+        "nombre": nombre,
+        "codigo": codigo,
+        "descripcion": descripcion,
+        "ubicacion": ubicacion
+        };
+        
+        comercios[codigo] = comercio;
+    }
+}
+
+function getComercios(){
+    return comercios;
+}
+
+function deleteComercio(codigo){
+    delete comercios[codigo]
+}
