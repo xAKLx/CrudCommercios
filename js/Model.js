bc3aada939555da1b768d15680adc39100b30e90
xAKLx <xaklx@hotmail.com>
2016-07-19 21:12:04 -0400

    Add updatable table

--- js/Model.js
+++ js/Model.js
@@ -1,10 +1,23 @@
+/*
+{
+    "Codigo": {
+        "nombre": string,
+        "codigo": string,
+        "descripcion": string,
+        "ubicacion": {
+            "lat": int,
+            "lng": int
+        }
+    }
+}
+*/
 var comercios = {};
 
 //nombre, codigo y ubicacion son requeridos
 //ubicacion debe tener como atributos lat y lng
 function addComercio(nombre, codigo, descripcion, ubicacion) {
     
-    if (nombre !== undefined && codigo !== undefined && ubicacion !== undefined && ubicacion.lat !== undefined && ubicacion.lng !== undefined) {
+    if (comercios[codigo] === undefined && nombre !== undefined && codigo !== undefined && ubicacion !== undefined && ubicacion.lat !== undefined && ubicacion.lng !== undefined) {
         var comercio = {
         "nombre": nombre,
         "codigo": codigo,
