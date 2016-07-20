328b87675ef11852c6976d7346759986d9600ce4
xAKLx <xaklx20@gmail.com>
2016-07-20 13:44:40 -0400

    Add map and fields to the modal

--- js/ComercioModal.js
+++ js/ComercioModal.js
@@ -1,3 +1,10 @@
 function saveComercio(){
-
+    var nombre = document.getElementById("inputNombre").value;
+    var codigo = document.getElementById("inputCodigo").value;
+    var descripcion = document.getElementById("inputDescripcion").value;
+    
+    var ubicacion = {"lat": 10, "lng": 10};
+    
+    addComercio(nombre, codigo, descripcion, ubicacion);
+    
 }
